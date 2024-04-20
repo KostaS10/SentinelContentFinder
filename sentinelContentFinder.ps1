e868f9eb95de8d503a570bd2b55771f916481ca2
Kosta S <43343162+KostaS10@users.noreply.github.com>
2024-04-20 21:55:22 +0200

    Update sentinelContentFinder.ps1

--- sentinelContentFinder.ps1
+++ sentinelContentFinder.ps1
@@ -18,7 +18,7 @@
     .NOTES
         AUTHOR= Kosta Sotic
         VERSION= 1.0.0
-        LASTUPDATE= 19/04/2024
+        LASTUPDATE= 20/04/2024
     .EXAMPLE
         If you want to check multiple tables, you need to call from pwsh like this (put in your variables): pwsh.exe -Command .\sentinelContentFinder.ps1 -TenantId '' -subscriptionId '' -WorkspaceName '' -ResourceGroupName '' -TableNames "WindowsEvent,SecurityEvent"
         For checking one table, you can call directly: .\sentinelContentFinderFinder.ps1 -TenantId '' -subscriptionId '' -WorkspaceName '' -ResourceGroupName '' -TableNames 'WindowsEvent'
